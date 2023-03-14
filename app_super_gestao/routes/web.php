8c4f222f02b3c9c57ebee5f016a54d3e06b83493
Hiury Lima <limahiury6@gmail.com>
2023-03-14 15:27:26 -0300

    Melhorando o visual de algumas views.

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function (){ return 'Login'; })->name('site.login');
+
+Route::prefix('/app')->group(function (){
+    Route::get('/clientes', function (){ return 'clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', [\App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', function (){ return 'produtos'; })->name('app.produtos');
+});
