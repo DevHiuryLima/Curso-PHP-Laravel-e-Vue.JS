572375965d8e5df09a0ae7bf8e6f313bd2d2fd19
Hiury Lima <limahiury6@gmail.com>
2023-03-17 17:27:56 -0300

    Melhorando a organização das views no blade template.

--- app_super_gestao/resources/views/site/contato.blade.php
+++ app_super_gestao/resources/views/site/contato.blade.php
@@ -1,21 +1,7 @@
 @extends('site.layouts.basico')
+@section('titulo', 'Contato')
 
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="{{ asset('img/logo.png') }}">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
-
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
             <h1>Entre em contato conosco</h1>
