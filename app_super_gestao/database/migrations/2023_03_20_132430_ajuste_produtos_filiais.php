e4481bb9ab454de5a198bac4f58d45d6e17f5569
Hiury Lima <limahiury6@gmail.com>
2023-03-21 14:04:17 -0300

    Criando as migrations para a gestão de produtos.

---
+++ app_super_gestao/database/migrations/2023_03_20_132430_ajuste_produtos_filiais.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('filiais', function (Blueprint $table) {
+            $table->id();
+            $table->string('filiais', 30);
+            $table->timestamps();
+        });
+
+        Schema::create('produto_filiais', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('filial_id');
+            $table->unsignedBigInteger('produto_id');
+            $table->decimal('preco_venda', 8, 2)->default(0.01);
+            $table->integer('estoque_minimo')->default(1);
+            $table->integer('estoque_maximo')->default(1);
+            $table->timestamps();
+
+            // Foreign key (constraints)
+            $table->foreign('filial_id')->references('id')->on('filiais');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+        });
+
+        // removendo coluna da tabela produtos
+        Schema::table('produtos', function (Blueprint $table) {
+           $table->dropColumn(['preco_venda', 'estoque_minimo', 'estoque_maximo']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // removendo coluna da tabela produtos
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->decimal('preco_venda', 8, 2)->default(0.01);
+            $table->integer('estoque_minimo')->default(1);
+            $table->integer('estoque_maximo')->default(1);
+        });
+
+        Schema::dropIfExists('produtos_filiais');
+        Schema::dropIfExists('filiais');
+    }
+};
